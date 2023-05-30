6dbd1f289ac323008a21ecc1f42b2f1f750f221c
Canterer <1170836331@qq.com>
2023-05-30 15:32:21 +0800

    new navigation project

---
+++ BaseNavigation/Source/BaseNavigation/BaseNavigationGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BaseNavigationGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABaseNavigationGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABaseNavigationGameMode();
+};
+
+
+
