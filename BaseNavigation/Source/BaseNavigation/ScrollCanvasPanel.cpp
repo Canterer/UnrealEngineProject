b85c062512885d3d92218376cdc4b9fd55b8ca76
Canterer <1170836331@qq.com>
2023-06-05 18:25:43 +0800

    scale and move panel

---
+++ BaseNavigation/Source/BaseNavigation/ScrollCanvasPanel.cpp
@@ -0,0 +1,91 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ScrollCanvasPanel.h"
+#include "Blueprint/WidgetLayoutLibrary.h"
+
+TSharedRef<SWidget> UScrollCanvasPanel::RebuildWidget()
+{
+    Super::RebuildWidget();
+    MyCanvas->SetOnMouseButtonDown(BIND_UOBJECT_DELEGATE(FPointerEventHandler, OnMouseButtonDown));
+	MyCanvas->SetOnMouseButtonUp(BIND_UOBJECT_DELEGATE(FPointerEventHandler, OnMouseButtonUp));
+	MyCanvas->SetOnMouseMove(BIND_UOBJECT_DELEGATE(FPointerEventHandler, OnMouseMove));
+	//MyCanvas->SetOnMouseEnter(BIND_UOBJECT_DELEGATE(FNoReplyPointerEventHandler, OnMouseEnter));
+	MyCanvas->SetOnMouseLeave(BIND_UOBJECT_DELEGATE(FSimpleNoReplyPointerEventHandler, OnMouseLeave));
+    return MyCanvas.ToSharedRef();
+}
+
+FReply UScrollCanvasPanel::OnMouseButtonDown(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
+{
+    int pointIndex = MouseEvent.GetPointerIndex();
+    FVector2D pos = MouseEvent.GetScreenSpacePosition();
+    FVector2D local = MyGeometry.LocalToAbsolute(FVector2D(0, 0));
+    const float scale = UWidgetLayoutLibrary::GetViewportScale(this);
+    //UE_LOG(LogTemp, Log, TEXT("OnMouseButtonDown FVector2D: %f %f local: %f %f scale: %f"), pos.X, pos.Y, local.X, local.Y, scale);
+    pointerDownPos = (pos - local) / scale;
+    //UE_LOG(LogTemp, Log, TEXT("OnMouseButtonDown pointIndex:%d FVector2D: %f %f"), pointIndex, pos.X, pos.Y);
+    bNeedMouseMove = true;
+    pointerPosMap.Add(pointIndex, pos);
+    if (pointerPosMap.Num() >= 2)
+    {//缩放为触屏双指事件 pointIndex为0 1
+        scaleVecLength = FVector2D::Distance(pointerPosMap[0], pointerPosMap[1]);
+        scaleCenter = (pointerPosMap[0] + pointerPosMap[1])/scale/2;
+        //UE_LOG(LogTemp, Log, TEXT("scaleCenter FVector2D: %f %f FVector2D: %f %f"), scaleCenter.X, scaleCenter.Y, pointerPosMap[0].X, pointerPosMap[0].Y, pointerPosMap[1].X, pointerPosMap[1].Y);
+        onScaleEvent.Broadcast(scaleCenter, 1, true);
+    }
+
+    return FReply::Handled();
+}
+FReply UScrollCanvasPanel::OnMouseButtonUp(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
+{
+    int pointIndex = MouseEvent.GetPointerIndex();
+    FVector2D pos = MouseEvent.GetScreenSpacePosition();
+    FVector2D local = MyGeometry.LocalToAbsolute(FVector2D(0, 0));
+    const float scale = UWidgetLayoutLibrary::GetViewportScale(this);
+    UE_LOG(LogTemp, Log, TEXT("OnMouseButtonUp FVector2D: %f %f local: %f %f scale: %f"), pos.X, pos.Y, local.X, local.Y, scale);
+    pos = (pos - local) / scale;
+    bNeedMouseMove = false;
+    pointerPosMap.Remove(pointIndex);
+    UE_LOG(LogTemp, Log, TEXT("OnMouseButtonUp pointIndex:%d FVector2D: %f %f"), pointIndex, pos.X, pos.Y);
+    if (FVector2D::Distance(pos, pointerDownPos) < 20)
+    {
+        onClickEvent.Broadcast(pos);
+    }else
+        UE_LOG(LogTemp, Log, TEXT("OnMouseButtonUp pointIndex:%d distance:%f FVector2D: %f %f FVector2D: %f %f"), pointIndex, FVector2D::Distance(pos, pointerDownPos), pointerDownPos.X, pointerDownPos.Y, pos.X, pos.Y);
+    return FReply::Handled();
+}
+
+void UScrollCanvasPanel::OnMouseEnter(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
+{
+    int pointIndex = MouseEvent.GetPointerIndex();
+    FVector2D pos = MouseEvent.GetScreenSpacePosition();
+    pointerPosMap.Remove(pointIndex);
+    //UE_LOG(LogTemp, Log, TEXT("OnMouseEnter pointIndex:%d FVector2D: %f %f"), pointIndex, pos.X, pos.Y);
+}
+FReply UScrollCanvasPanel::OnMouseMove(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent)
+{
+    if(!bNeedMouseMove)
+        return FReply::Unhandled();
+    int pointIndex = MouseEvent.GetPointerIndex();
+    FVector2D pos = MouseEvent.GetScreenSpacePosition();
+    FVector2D tempLastPos = pointerPosMap[pointIndex];
+    pointerPosMap[pointIndex] = pos;
+    if( pointerPosMap.Num() >= 2)
+    {//缩放为触屏双指事件 pointIndex为0 1
+        float vecLength = FVector2D::Distance(pointerPosMap[0], pointerPosMap[1]);
+        //UE_LOG(LogTemp, Log, TEXT("OnMouseMove scale: %f %f %f"), vecLength/scaleVecLength, scaleVecLength, vecLength);
+        onScaleEvent.Broadcast(scaleCenter, vecLength/scaleVecLength, false);
+    }else if(bNeedMouseMove)
+    {
+        //UE_LOG(LogTemp, Log, TEXT("OnMouseMove"));
+        onMoveEvent.Broadcast(pos - tempLastPos);
+    }
+    return FReply::Handled();
+}
+void UScrollCanvasPanel::OnMouseLeave(const FPointerEvent& MouseEvent)
+{
+    int pointIndex = MouseEvent.GetPointerIndex();
+    FVector2D pos = MouseEvent.GetScreenSpacePosition();
+    //UE_LOG(LogTemp, Log, TEXT("OnMouseLeave pointIndex:%d FVector2D: %f %f"), pointIndex, pos.X, pos.Y);
+    bNeedMouseMove = false;
+}
---
+++ BaseNavigation/Source/BaseNavigation/ScrollCanvasPanel.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/CanvasPanel.h"
+#include "ScrollCanvasPanel.generated.h"
+
+/**
+ * 
+ */
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnClickEvent, FVector2D, pos);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMoveEvent, FVector2D, moveVec);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnScaleEvent, FVector2D, scaleCenter, float, scaleRate, bool, bStart);
+UCLASS()
+class BASENAVIGATION_API UScrollCanvasPanel : public UCanvasPanel
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(BlueprintAssignable)
+		FOnClickEvent onClickEvent;
+	UPROPERTY(BlueprintAssignable)
+		FOnMoveEvent onMoveEvent;
+	UPROPERTY(BlueprintAssignable)
+		FOnScaleEvent onScaleEvent;
+
+
+	virtual FReply OnMouseButtonDown(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent);
+	virtual FReply OnMouseButtonUp(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent);
+
+	virtual void OnMouseEnter(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent);
+	virtual FReply OnMouseMove(const FGeometry& MyGeometry, const FPointerEvent& MouseEvent);
+	virtual void OnMouseLeave(const FPointerEvent& MouseEvent);
+
+protected:
+	// UWidget interface
+	virtual TSharedRef<SWidget> RebuildWidget() override;
+	FVector2D scaleCenter;
+	float scaleVecLength;
+	FVector2D pointerDownPos;
+	TMap<int, FVector2D> pointerPosMap;
+	bool bNeedMouseMove = false;
+};
